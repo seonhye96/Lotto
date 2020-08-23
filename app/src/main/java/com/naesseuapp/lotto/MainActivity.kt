f872c272127b8dd630a095ca959d61d243466d5f
황선혜 <ghkdtjs6451@gmail.com>
2020-08-24 01:12:07 +0900

    랜덤번호 생성

--- app/src/main/java/com/naesseuapp/lotto/MainActivity.kt
+++ app/src/main/java/com/naesseuapp/lotto/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +14,12 @@ class MainActivity : AppCompatActivity() {
 
 //        랜덤으로 번호 생성 카드를 클릭 이벤트 리스너
         randomCard.setOnClickListener {
-//            ResultActivity를 시작하는 Intent 를 만들고 startActivity로 실행
-            startActivity(Intent(this, ResultActivity::class.java))
+
+            val intent = Intent(this, ResultActivity::class.java)
+
+            intent.putIntegerArrayListExtra("result", ArrayList(getRandomLottoNumbers()))
+
+            startActivity(intent)
         }
 
 //        별자리로 번호 생성 카드를 클릭 이벤트 리스너
@@ -29,5 +35,35 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+    }
+
+    //    랜덤으로 1~45 번중 하나의 번호를 생성하는 함수
+    fun getRandomLottoNumber() : Int {
+        //Random.nextInt 는 0~전달받은 파라미터 값 미만의 번호를 생성
+        // ex) Random().nextInt(10) 은 0~9 까지의 무작위 수를 반환
+        // 1~45 까지의 번호를 생성하려면 파라미터의 45를 넣고 결과값의 1을 더한다.
+        return Random().nextInt(45)+1
+    }
+
+    // 랜덤으로 추출하여 6개의 로또 번호를 만드는 함수
+    fun getRandomLottoNumbers() : MutableList<Int>{
+        //무작위로 생성된 로또 번호를 저장할 가변 리스트 생성
+        val lottoNumbers = mutableListOf<Int>()
+
+        // 6번 반복하는 for문
+        for (i in 1..6){
+            var number = 0
+            do{
+                //랜덤한 번호를 추출해 number 변수에 저장
+                number = getRandomLottoNumber()
+
+                // lottoNumbers 에 number 변수의 값이 없을 때까지 반복
+            }while (lottoNumbers.contains(number))
+
+            // 이미 뽑은 리스트에 없는 번호가 나올때까지 반복했으므로 중복이 없는 상태
+            // 추출된 번호를 뽑은 리스트에 추가
+            lottoNumbers.add(number)
+        }
+        return lottoNumbers
     }
 }
--- app/src/main/java/com/naesseuapp/lotto/ResultActivity.kt
+++ app/src/main/java/com/naesseuapp/lotto/ResultActivity.kt
@@ -2,10 +2,34 @@ package com.naesseuapp.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
+
+    val lottoImageStartId = R.drawable.num1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val result = intent.getIntegerArrayListExtra("result")
+
+        // 전달받은 결과가 있는 경우에만 실행
+        result?.let {
+            // 결과에 맞게 로또 공 이미지를 업데이트한다.
+            // 전달받은 결과는 정렬되지 있지 않으므로 정렬해서 전달한다.
+            updateLottoBallImage(result)
+        }
+    }
+
+    fun updateLottoBallImage(result:List<Int>){
+        if(result.size < 6) return
+
+        imageView01.setImageResource(lottoImageStartId + (result[0] - 1))
+        imageView02.setImageResource(lottoImageStartId + (result[1] - 1))
+        imageView03.setImageResource(lottoImageStartId + (result[2] - 1))
+        imageView04.setImageResource(lottoImageStartId + (result[3] - 1))
+        imageView05.setImageResource(lottoImageStartId + (result[4] - 1))
+        imageView06.setImageResource(lottoImageStartId + (result[5] - 1))
     }
 }
