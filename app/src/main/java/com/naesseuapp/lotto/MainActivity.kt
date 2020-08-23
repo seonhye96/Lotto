def99f763abe0b2ac199aedcf791bad865ee059e
황선혜 <ghkdtjs6451@gmail.com>
2020-08-24 00:26:58 +0900

    로또 Intent 추가  + 랜덤번호 UI 변경

--- app/src/main/java/com/naesseuapp/lotto/MainActivity.kt
+++ app/src/main/java/com/naesseuapp/lotto/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.naesseuapp.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        랜덤으로 번호 생성 카드를 클릭 이벤트 리스너
+        randomCard.setOnClickListener {
+//            ResultActivity를 시작하는 Intent 를 만들고 startActivity로 실행
+            startActivity(Intent(this, ResultActivity::class.java))
+        }
+
+//        별자리로 번호 생성 카드를 클릭 이벤트 리스너
+        constellationCard.setOnClickListener {
+//            ConstellationActivity 를 시작하는 Intent 를 만들고 startActivity로 실행
+            startActivity(Intent(this, ConstellationActivity::class.java))
+        }
+
+//        이름으로 번호 생성 카드를 클릭 이벤트 리스너
+        nameCard.setOnClickListener {
+//            NameActivity를 시작하는 Intent를 만들고 startActivity로 실행
+            startActivity(Intent(this, NameActivity::class.java))
+        }
+
+
     }
 }
--- app/src/main/java/com/naesseuapp/lotto/NameActivity.kt
+++ app/src/main/java/com/naesseuapp/lotto/NameActivity.kt
@@ -1,11 +1,25 @@
 package com.naesseuapp.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_constellation.*
+import kotlinx.android.synthetic.main.activity_name.*
 
 class NameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name)
+
+        // 로또 번호 확인 버튼의 클릭이벤트 리스너 설정
+        goButton.setOnClickListener{
+            // ResultActivity 를 시작하는 인텐트를 만들고 startActivity 로 실행
+            startActivity(Intent(this, ResultActivity::class.java))
+        }
+
+        // 뒤로가기 버튼의 클릭이벤트 리스너 설정
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
